af1dd85c1a51819b97f34b193662c02f90e88075
curlynux <smpunchack@gmail.com>
2017-09-21 05:24:24 +0200

    more info in the if statement

--- code.py
+++ code.py
@@ -2,17 +2,16 @@ import requests
 from bs4 import BeautifulSoup as bfs
 
 def get_url(url):
-    for n in range(220, 750):
+    for n in range(0, 1000):
         dir_url = url + format(n) + "/"
         r = requests.get(dir_url).content
         soup = bfs(r, "html.parser")
-        div = soup.find("div", {"id" : "wallwindow"})
-        imgs = div.findChildren()
+        imgs = soup.find("div", {"id" : "wallwindow"}).findChildren()
         for img in imgs:
             src = img.get('src')
             if (not "WP_" in src):
-                continue
+                print "no img in the dir number: %d" % n
             else:
-                print src
+                print "img number: %d, src: %s" % (n, src)
 
 get_url("http://www.justinmaller.com/wallpaper/")
